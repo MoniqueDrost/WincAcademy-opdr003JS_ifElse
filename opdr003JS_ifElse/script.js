59ea692be133e30817d3b05618182ff3c4e0e1ec
MoniqueDrost <80634091+MoniqueDrost@users.noreply.github.com>
2021-05-05 16:32:15 +0200

    Add files via upload

---
+++ opdr003JS_ifElse/script.js
@@ -0,0 +1,24 @@
+
+const age = 15;
+const isFemale = false;
+const driverStatus = "niet de bob";
+
+if (age >= 18) {
+    console.log("Welkom, je mag binnen komen");
+
+} else {
+    console.log("Helaas, je mag hier niet naar binnen");
+}
+
+
+if (isFemale) {
+    console.log("Gefeliciteerd, je mag een kaartje kopen")
+} else {
+    console.log("Helaas, deze avond is alleen voor vrouwen")
+}
+
+if (driverStatus == "bob") {
+    console.log("Jij mag naar huis rijden")
+} else {
+    console.log("Sorry, jij moet een taxi bellen")
+}
